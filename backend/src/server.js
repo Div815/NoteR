9dd83c4740f638aa47d4bb2ac8897db29c749862
Div815 <vdivyesh821@gmail.com>
2026-04-01 16:12:32 +0530

    removed an secret text

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,4 +7,3 @@ app.use("/api/notes",notesRoutes)
 
 app.listen(5001,()=>{ console.log('Server is running on port 5001') })
 
-// mongodb+srv://vdivyesh821_db_user:VjDuzQwuAvc5RQH6@cluster0.d0zctnx.mongodb.net/?appName=Cluster0
