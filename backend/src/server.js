73b8dbf21786918243259972fccaad22cdff6ac1
Div815 <vdivyesh821@gmail.com>
2026-04-21 21:16:34 +0530

    Final commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,19 @@ import {connectDB} from './config/db.js'
 import 'dotenv/config'
 import rateLimiter from './Middleware/rateLimiter.js'
 import cors from 'cors'
+import path from 'path'
+
 const app=express()
+
+const __dirname=path.resolve();
 
 const PORT = process.env.PORT || 5001
 //Middleware
+if (process.env.NODE_ENV !== "production"){
 app.use(cors({
     origin: "http://localhost:5173"
 }));
+}
 
 app.use(express.json())
 app.use(rateLimiter) // Apply the rate limiter middleware to all routes
@@ -20,6 +26,16 @@ app.use((req,res,next)=>{
     next(); 
 })
 app.use("/api/notes",notesRoutes)
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+} );
+}
+
+
 connectDB().then(()=>{
 app.listen(PORT,()=>{ console.log(`Server is running on port ${PORT}`) })
 
