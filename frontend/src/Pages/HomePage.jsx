94b70cfd9a0a4ae121ab71232947127a30a29c32
Div815 <vdivyesh821@gmail.com>
2026-04-22 12:53:56 +0530

    ,nm

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -49,9 +49,13 @@ function HomePage() {
 
         {notes.length >0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {notes.map(note =>(
-              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
-            ))}
+            {Array.isArray(notes) ? (
+  notes.map(note => (
+    <NoteCard key={note._id} note={note} setNotes={setNotes}/>
+  ))
+) : (
+  <p>Error: Data is not a list of notes.</p>
+)}
 
           </div>
         )}
