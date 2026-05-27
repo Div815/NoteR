31d15a8face8eb99e82aa15325c8f8c2498fbeba
Div815 <vdivyesh821@gmail.com>
2026-05-27 15:57:33 +0530

    rebooted back to the render configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,7 +27,13 @@ app.use((req,res,next)=>{
 })
 app.use("/api/notes",notesRoutes)
 
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+} );
+}
 
 
 connectDB().then(()=>{
